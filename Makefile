3c834e7e7666a9bd1306327b67e3faa914b2cefc
Andrew Molis <ajmolis@dons.usfca.edu>
2023-02-24 13:04:17 -0800

    Added simple test

--- Makefile
+++ Makefile
@@ -8,17 +8,17 @@ conn=connection
 INC=include
 
 lib_ob=$(src)/$(conn)/tcp_sock_handler.o
-OBJ=$(test_dir)/test_tcp_sock_handler.o $(test_dir)/simple_client.o $(test_dir)/simple_server.o
+OBJ=$(test_dir)/test_tcp_sock_handler.o $(test_dir)/simple_client.o $(test_dir)/simple_server.o $(test_dir)/test_simple_client_server.o
 test_bin=$(test_dir)/test_tcp_sock_handler $(test_dir)/simple_client $(test_dir)/simple_server
 test_run=$(test_dir)/test_runner.sh
-
-exe=$(addprefix ./, $(test_bin))
+test_files=$(test_dir)/test_simple_client_server
+exe=$(addprefix ./, $(test_files))
 
 all: $(test_bin)
 
 simple_server: $(test_dir)/simple_server
 
-test: $(test_bin)
+test: $(test_files)
 	sh $(test_run) $(exe)
 
 $(OBJ): %.o: %.c
@@ -28,6 +28,9 @@ $(OBJ): $(lib_ob)
 
 $(test_bin): %: %.o
 	 $(CC) $(CFLAGS) $(lib_ob) $^ -o $@
+
+$(test_files): %: %.o
+	$(CC) $(CFLAGS) $(lib_ob) $^ -o $@
 
 clean:
 	-rm $(lib_ob)
---
+++ test/test_simple_client_server.c
@@ -0,0 +1,92 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "tcp_sock_handler.h"
+
+
+struct args {
+    char *host;
+    char *port;
+    char *input;
+    char output[1024];
+};
+
+void *run_server(void *inputs)
+{
+  struct args *server_args = (struct args*) inputs;
+  unsigned short server_port = atoi (server_args->port);
+
+  TCP_SERVER server = tcp_new_server (server_port);
+  int started = tcp_server_start (server);
+  printf("started %d\n", started);
+  if (started)
+    {
+      perror ("Unable to start server");
+      abort ();
+    }
+  TCP_HANDLER client_handler = tcp_server_next_connection (server);
+  printf("Sending data on server\n");
+  int sent = tcp_sendn(client_handler, server_args->input, 6);
+  printf("Server sent data to client on port %d\n", server_port);
+  if (sent)
+    printf("server failed to send hello from server\n");
+  else
+    printf("sent Hello from server\n");
+
+  if (destroy_tcp_handler (client_handler))
+    printf("Failed to destroy client socket handler in server\n");
+  if (destroy_tcp_sever (server))
+    printf ("Failed to destroy server handler");
+
+  return 0;
+}
+
+void *run_client(void *inputs)
+{
+  struct args *server_args = (struct args*) inputs;
+  printf ("Running test with server %s on port %s in thread\n", server_args->host, server_args->port);
+
+  unsigned short port = atoi (server_args->port);
+  TCP_CLIENT_CONN client = tcp_new_client (server_args->host, port);
+  int connected = tcp_client_connect(client);
+  if (connected)
+    {
+      printf ("Unable to connect to server %s on port %d\n", server_args->host, port);
+      abort ();
+    }
+  char test[6] = {'\0'};
+  int received = tcp_recvn (client->handler, test, 6);
+  if (!received)
+    printf("Client failed to receive message from server from server %s\n", test);
+  else
+    printf("Client received message from server: %s\n", test);
+  if (destroy_tcp_client (client))
+    printf("Failed to destroy client socket handler\n");
+  printf ("Copying data test: %s output: %s\n", test, server_args->output);
+  strcpy (server_args->output, test);
+  printf ("Copying data test: %s output: %s\n", test, server_args->output);
+  return 0;
+}
+
+int main() {
+  pthread_t client_thread, server_thread;
+  struct args *server_args = malloc (sizeof (struct args));
+  server_args->port =  "12055";
+  server_args->host =  "localhost";
+  server_args->input =  "hello";
+
+  pthread_create (&server_thread, NULL, (void *) &run_server, (void *) server_args);
+  sleep (1);
+  pthread_create (&client_thread, NULL, (void *) &run_client, (void *) server_args);
+  pthread_join(client_thread, NULL);
+  pthread_join(server_thread, NULL);
+  int success = 1;
+  if (strcmp(server_args->input, server_args->output) == 0)
+    success = 0;
+  printf ("Copying data input: %s output: %s\n", server_args->input, server_args->output);
+  free(server_args);
+  printf("Success = %d\n", success);
+  return success;
+}
